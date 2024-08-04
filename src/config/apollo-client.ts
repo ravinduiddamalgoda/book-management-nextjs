6056dfd7c1d0ebf9691ed542eaa3044c4708b118
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2024-08-05 03:32:38 +0530

    variable

--- src/config/apollo-client.ts
+++ src/config/apollo-client.ts
@@ -7,18 +7,20 @@ import {
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client';
+
 // Function to create an Apollo Client instance and connect to the GraphQL backend
 const gqlClientConnect = () => {
   // Create a new InMemoryCache instance
   const cache = new InMemoryCache({
     addTypename: false,
     typePolicies: {},
-  });  
-// Create an HttpLink instance to connect to the GraphQL server
+  });
+
+  // Create an HttpLink instance to connect to the GraphQL server
   const httpLink = createHttpLink({
-    uri: 'http://localhost:3002/graphql',
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI, // Use the URI from the environment variable
     useGETForQueries: false, // Use GET method for queries
-  }); 
+  });
 
   const authLink = new ApolloLink((operation, forward) => {
     // Retrieve the token from localStorage
@@ -33,14 +35,17 @@ const gqlClientConnect = () => {
 
     return forward(operation);
   });
+
   const link = ApolloLink.from([authLink, httpLink]);
-// Create the ApolloClient instance
+
+  // Create the ApolloClient instance
   const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     ssrMode: typeof window === 'undefined', // Set SSR mode based on whether the window object is available
-    cache, // Assign the previously created InMemoryCache instance
-    // link: httpLink, // Assign the HttpLink instance as the link for the ApolloClient
+    cache,
     link,
   });
+
   return apolloClient; // Return the ApolloClient instance
 };
+
 export default gqlClientConnect;
